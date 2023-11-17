09c4ce7f3521cd1586b49a3741fbf66b800accb4
ahmadou5 <ahmadlasauwal@gmail.com>
2023-11-17 21:40:03 +0300

    'kkk0044'

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,10 @@
 import '@/styles/globals.css'
+import EthProvider from '@/pages/web3Provider'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <EthProvider>
+      <Component {...pageProps} />
+    </EthProvider>
+  )
 }
